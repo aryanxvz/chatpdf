b2a4018209e6995963b6e1fd4e629caf1479d663
Aryan Mane <aryanmane7916@gmail.com>
2025-01-05 18:57:17 +0530

    feat: layout and theme added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Header } from "@/components/sections/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +21,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{children: React.ReactNode;}>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Header />
           {children}
         </ThemeProvider>
       </body>
---
+++ src/components/sections/header.tsx
@@ -0,0 +1,12 @@
+import { DarkModeToggle } from "../ui/theme-button"
+
+export const Header = () => {
+  return (
+    <div className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-blur:bg-background/60">
+      <div className="flex h-20 items-center justify-between px-6 lg:px-12 shadow-xl">
+        <span className="text-3xl font-bold">ChatPDF</span>
+        <span><DarkModeToggle /></span>
+      </div>
+    </div>
+  )
+}
