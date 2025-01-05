1cbff1eb7ae7f95779618e201cbf0a5aece99ac7
Aryan Mane <aryanmane7916@gmail.com>
2025-01-05 19:03:21 +0530

    feat: added header

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -1,6 +1,6 @@
 import { DarkModeToggle } from "../ui/theme-button"
 
-export const Header = () => {
+export default function Header() {
   return (
     <div className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-blur:bg-background/60">
       <div className="flex h-20 items-center justify-between px-6 lg:px-12 shadow-xl">
