5d46e16572a0f8bcca8f1eea273a63348bb43410
Aryan Mane <aryanmane7916@gmail.com>
2025-01-08 23:39:23 +0530

    feat: chat line component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -46,7 +46,7 @@ export const initialMessages: Message[] = [
     role: "assistant",
     id: "0",
     content:
-      "Hi! I am your PDF assistant. I am happy to help with your questions about your PDF about German law.",
+      "Hi! I am your PDF assistant. I am happy to help with your questions.",
   },
 ];
 
