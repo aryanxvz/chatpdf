b7749e1d0352ca2c25a23b2d8ec08f98afa9ccac
Aryan Mane <aryanmane7916@gmail.com>
2025-01-10 19:01:02 +0530

    feat: chat component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Chat } from "@/components/sections/chat";
 
 export default function Home() {
   return (
-    <div className="py-12 px-12">
-      Hello from ChatPDF
+    <div className="flex flex-1 py-12 px-12">
+      <div className="w-full">
+        <Chat />
+      </div>
     </div>
   );
 }
