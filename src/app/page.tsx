2ac0f129eec5174489aae86ab0d4d0525fdb5d66
Aryan Mane <aryanmane7916@gmail.com>
2025-02-13 18:17:59 +0530

    fix: chat page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { Chat } from "@/components/sections/chat";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 py-12 px-12">
-      <div className="w-full">
-        <Chat />
+    <main>
+      <div className="flex flex-1 py-12 px-12">
+        <div className="w-full">
+          <Chat />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
