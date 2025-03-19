f1971021537718387062c3a39aa5b83cb3b339e6
Aryan Mane <aryanmane7916@gmail.com>
2025-03-19 18:49:37 +0530

    fix: header design

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -3,11 +3,11 @@ import Link from "next/link"
 
 export default function Header() {
   return (
-    <div className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-blur:bg-background/60 shadow-md">
+    <div className="sticky top-0 z-50 w-full border-b dark:bg-neutral-950 bg-transparent backdrop-blur supports-backdrop-blur:bg-background/60 shadow-md">
       <div className="mx-auto max-w-6xl 2xl:max-w-7xl w-full">
         <div className="flex h-16 md:h-20 items-center justify-between px-4 sm:px-6 lg:px-8">
           <Link href="/" className="flex items-center">
-            <span className="text-2xl md:text-3xl font-bold text-foreground">chatpdf</span>
+            <span className="text-2xl md:text-3xl font-bold text-foreground dark:text-neutral-200 text-neutral-700">chatpdf</span>
           </Link>
           <div className="flex items-center gap-4">
             <DarkModeToggle />
