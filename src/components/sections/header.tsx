9315697431f87d51a01ddcef9ecea9c2a0d9b208
Aryan Mane <aryanmane7916@gmail.com>
2025-03-19 20:00:55 +0530

    fix: header

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <div className="sticky top-0 z-50 w-full border-b dark:bg-neutral-950 bg-transparent backdrop-blur supports-backdrop-blur:bg-background/60 shadow-md">
       <div className="mx-auto max-w-6xl 2xl:max-w-7xl w-full">
-        <div className="flex h-16 md:h-20 items-center justify-between px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 md:h-20 items-center justify-between px-6 lg:px-8">
           <Link href="/" className="flex items-center">
             <span className="text-2xl md:text-3xl font-bold text-foreground dark:text-neutral-200 text-neutral-700">chatpdf</span>
           </Link>
